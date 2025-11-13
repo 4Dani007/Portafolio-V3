53374fab1e4b3fab67f77402f961c51a1ff87b5a
4Dani007 <danielbm9993@gmail.com>
2025-11-13 14:08:38 -0500

    fix: added package.json and other setting files

---
+++ my-app/next-intl.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-intl').NextIntlConfig} */
+module.exports = {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+    messagesDirectory: './src/locales', // ruta relativa desde la raíz
+  };
